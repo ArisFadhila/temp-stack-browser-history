380a762192b4065b3b994e7c938422a6bbdcfd36
Aris Fadhila <151005943+ArisFadhila@users.noreply.github.com>
2024-10-20 20:30:26 +0700

    Update and rename Main.java to BrowserHistory.java

---
+++ src/main/java/com/github/affandes/kuliah/pm/BrowserHistory.java
@@ -0,0 +1,60 @@
+package com.github.affandes.kuliah.pm;
+
+import java.util.LinkedList;
+
+public class BrowserHistory {
+    private LinkedList<String> history;
+
+    public BrowserHistory() {
+        history = new LinkedList<>();
+    }
+
+    public void view() {
+        if (history.isEmpty()) {
+            System.out.println("History browser kosong.");
+            return;
+        }
+        System.out.println("History Browser (dari yang terbaru):");
+        for (int i = history.size() - 1; i >= 0; i--) {
+            System.out.println(history.get(i));
+        }
+    }
+
+    public void browse(String website) {
+        history.add(website); 
+        System.out.println("Mengunjungi: " + website);
+    }
+
+        if (history.isEmpty()) {
+            System.out.println("Tidak ada history untuk kembali.");
+            return;
+        }
+        String lastVisited = history.removeLast();
+        System.out.println("Kembali dari: " + lastVisited);
+    }
+
+    public static void main(String[] args) {
+        BrowserHistory browserHistory = new BrowserHistory();
+
+        browserHistory.browse("www.google.com");
+        browserHistory.browse("www.wikipedia.org");
+        browserHistory.browse("www.github.com");
+
+        browserHistory.view();
+        browserHistory.back(); 
+        browserHistory.view(); 
+
+        browserHistory.back();
+        browserHistory.view(); 
+
+        browserHistory.back(); 
+        browserHistory.view(); 
+
+        browserHistory.back(); 
+    }
+}
+
+        
+        
+
+    
